fcc38fb841b2e0ab3c6d3caf1dd71be048d7be4f
dhalbhav <52804176+dhalbhav@users.noreply.github.com>
2019-07-26 22:22:16 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,11 @@
+import {BookInfo} from '../../app/bookInfo';
+
+export const MYBOOKS: BookInfo[] =
+[
+  
+{bname: 'book1',bauthor: 'andrew', bgenre:'biography1', byear:'2014',bpicture:'book1.jpg'},
+{bname: 'book2',bauthor: 'Tony Robbins', bgenre:'biography2', byear:'2008',bpicture:'book2.jpg'},
+{bname: 'book3',bauthor: 'Vanessa Van Edwards', bgenre:'biography3', byear:'2000',bpicture:'book3.jpg'},
+{bname: 'book4',bauthor: 'Scott Adams', bgenre:'biography4', byear:'2003',bpicture:'book4.jpg'}
+
+]
